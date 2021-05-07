b758339990ce04cbd9a3ef43d72ae2246aac3590
DiegoAlonso1 <diegoalonsooliverab@gmail.com>
2021-05-07 16:05:36 -0500

    Starting Event & Hobbyist controllers

--- Events/src/main/java/com/perustars/events/controller/HobbyistsController.java
+++ Events/src/main/java/com/perustars/events/controller/HobbyistsController.java
@@ -1,4 +1,8 @@
 package com.perustars.events.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class HobbyistsController {
+    @Autowired
+    private ModelMapper mapper;
 }
